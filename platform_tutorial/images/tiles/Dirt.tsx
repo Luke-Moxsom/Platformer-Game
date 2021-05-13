273e0be880d0cfee0f6cf98c436774ebb9403681
luke <lukewillmoxsom@gmail.com>
2021-05-13 13:04:26 +1200

    hi
    hi

---
+++ platform_tutorial/images/tiles/Dirt.tsx
@@ -0,0 +1,112 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Dirt" tilewidth="128" tileheight="128" tilecount="36" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="dirt.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="dirtCenter_rounded.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="dirtCenter.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="128" source="dirtCliff_left.png"/>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="dirtCliff_right.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="128" source="dirtCliffAlt_left.png"/>
+ </tile>
+ <tile id="6">
+  <image width="128" height="128" source="dirtCliffAlt_right.png"/>
+ </tile>
+ <tile id="7">
+  <image width="128" height="128" source="dirtCorner_left.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="dirtCorner_right.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="dirtHalf_left.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="dirtHalf_mid.png"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="128" source="dirtHalf_right.png"/>
+ </tile>
+ <tile id="12">
+  <image width="128" height="128" source="dirtHalf.png"/>
+ </tile>
+ <tile id="13">
+  <image width="128" height="128" source="dirtHill_left.png"/>
+ </tile>
+ <tile id="14">
+  <image width="128" height="128" source="dirtHill_right.png"/>
+ </tile>
+ <tile id="15">
+  <image width="128" height="128" source="dirtLeft.png"/>
+ </tile>
+ <tile id="16">
+  <image width="128" height="128" source="dirtMid.png"/>
+ </tile>
+ <tile id="17">
+  <image width="128" height="128" source="dirtRight.png"/>
+ </tile>
+ <tile id="21">
+  <image width="128" height="128" source="grass.png"/>
+ </tile>
+ <tile id="22">
+  <image width="128" height="128" source="grassCenter_round.png"/>
+ </tile>
+ <tile id="23">
+  <image width="128" height="128" source="grassCenter.png"/>
+ </tile>
+ <tile id="24">
+  <image width="128" height="128" source="grassCliff_left.png"/>
+ </tile>
+ <tile id="25">
+  <image width="128" height="128" source="grassCliff_right.png"/>
+ </tile>
+ <tile id="26">
+  <image width="128" height="128" source="grassCliffAlt_left.png"/>
+ </tile>
+ <tile id="27">
+  <image width="128" height="128" source="grassCliffAlt_right.png"/>
+ </tile>
+ <tile id="28">
+  <image width="128" height="128" source="grassCorner_left.png"/>
+ </tile>
+ <tile id="29">
+  <image width="128" height="128" source="grassCorner_right.png"/>
+ </tile>
+ <tile id="30">
+  <image width="128" height="128" source="grassHalf_left.png"/>
+ </tile>
+ <tile id="31">
+  <image width="128" height="128" source="grassHalf_mid.png"/>
+ </tile>
+ <tile id="32">
+  <image width="128" height="128" source="grassHalf_right.png"/>
+ </tile>
+ <tile id="33">
+  <image width="128" height="128" source="grassHalf.png"/>
+ </tile>
+ <tile id="34">
+  <image width="128" height="128" source="grassHill_left.png"/>
+ </tile>
+ <tile id="35">
+  <image width="128" height="128" source="grassHill_right.png"/>
+ </tile>
+ <tile id="36">
+  <image width="128" height="128" source="grassLeft.png"/>
+ </tile>
+ <tile id="37">
+  <image width="128" height="128" source="grassMid.png"/>
+ </tile>
+ <tile id="38">
+  <image width="128" height="128" source="grassRight.png"/>
+ </tile>
+</tileset>
