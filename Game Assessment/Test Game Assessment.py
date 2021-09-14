0922276905b3509c11bfa143385894b6f31c288a
luke <lukewillmoxsom@gmail.com>
2021-09-14 19:00:49 +1200

    Title

--- Game Assessment/Test Game Assessment.py
+++ Game Assessment/Test Game Assessment.py
@@ -37,6 +37,7 @@ def load_texture_pair(filename):
     """ Load a texture pair, with the second being a mirror image """
     return [
         arcade.load_texture(filename),
+        arcade.load_texture(filename),
         arcade.load_texture(filename, flipped_horizontally=True)
     ]
 
